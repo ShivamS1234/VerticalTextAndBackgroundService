e875e85a1c837600efc14edaa6c77f0fb94800c2
ShivamS1234 <shivammcu@gmail.com>
2018-09-25 15:25:08 +0530

    first commit

--- DemoBackgroundService/MainPage.xaml.cs
+++ DemoBackgroundService/MainPage.xaml.cs
@@ -12,6 +12,38 @@ namespace DemoBackgroundService
         public MainPage()
         {
             InitializeComponent();
+            try
+            {
+                string name = "SHIVAM";
+                var grid = new Grid
+                {
+                    HorizontalOptions = LayoutOptions.CenterAndExpand,
+                    VerticalOptions = LayoutOptions.CenterAndExpand,
+                    ColumnDefinitions =
+                {
+                    new ColumnDefinition { Width = GridLength.Auto }
+                }
+                };
+                grid.RowDefinitions = new RowDefinitionCollection();
+
+                for (int MyCount = 0; MyCount < name.Length; MyCount++)
+                {
+
+                    grid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
+                }
+                foreach (char chr in name)
+                {
+                    grid.Children.Add(new Label { Text = chr.ToString()  }, 0, name.IndexOf(chr));
+                }
+             
+                this.Content = grid;
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+         
         }
     }
 }
